2a36e8252db324a0f097227cb98ef71451cd3e9d
pedrohsenas <projetoseletrica.uis3@gmail.com>
2026-07-12 15:45:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
    Deixe em branco para rodar 100% local.
    ============================================================ */
 window.CONFIG_NUVEM = {
-  url: "https://oxhyvaqhbdtvmqvxlqdn.supabase.co/rest/v1/",
+  url: "https://oxhyvaqhbdtvmqvxlqdn.supabase.co/",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94aHl2YXFoYmR0dm1xdnhscWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NzE0MDAsImV4cCI6MjA5OTQ0NzQwMH0.qYcF-EvTVLL-bP0gs8-fZckMsgqVxvUUykwTynvZbgA", 
 };
